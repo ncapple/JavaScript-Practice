3dce640ab07a71ecb4bdee33b353d7c213608e4f
Neal <ncapple@kent.edu>
2018-01-18 12:33:43 -0800

    JS practice

---
+++ declare.js
@@ -0,0 +1,11 @@
+var favnumb= "2";
+var leastfavnumb= "3";
+var numb="1";
+var numb2="3335984";
+var string2=[1,2,3,4];
+var string3=[1,3,5,7];
+var sting4=[2,4,6,8];
+var JSisHard="True";
+var JSisEasy="True";
+
+console.log(numb2);
